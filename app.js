88833e0c5eacd7a6a264a28b806fbd8be1b729f6
FarazShasm981 <hafizfaraz981@gmail.com>
2025-09-04 13:38:00 +0500

    initial commit

---
+++ app.js
@@ -0,0 +1,193 @@
+//! Chapter # 01 
+
+//Question # 01 Start
+// alert("Hey User! Welcome to Our Website")
+//Question # 01 End
+
+//Question # 02 Start
+// alert("Error! Please Enter A Valid Password")
+//Question # 02 End
+
+//Question # 03 Start
+// alert("Welcome To JS land...\nHappy Coding")
+//Question # 03 End
+ 
+//Question # 04 Start
+// alert("Welcome to JS land...")
+// alert("Happy Coding!")
+//Question # 04 End
+
+//Question # 05 Start
+// console.log("Hello...I can run JS through my web browser`s console");
+//Question # 05 End
+
+// Question # 06 and 07 done.......
+
+
+//! Chapter # 02
+
+//Question # 01 Start
+// var userName;
+//Question # 01 End
+
+//Question # 02 Start
+// var myName = "Faraz Shams"
+//Question # 02 End
+
+//Question # 03 Start
+// var message = "Hello World";
+// alert(message);
+//Question # 03 End
+
+//Question # 04 Start
+// var studentName = "John Doe";
+// var studentAge = "15 years old";
+// var studentDesig = "Certified Mobile Application Development";
+// alert(studentName)
+// alert(studentAge)
+// alert(studentDesig)
+//Question # 04 End
+
+//Question # 05 Start
+// var pizza = "PIZZA\nPIZZ\nPIZ\nPI\nP"
+// alert(pizza)
+//Question # 05 End
+
+//Question # 06 Start
+// var email = "farazshams981@gmail.com";
+// alert("My Email Address is : "+ email)
+//Question # 06 End
+
+//Question # 07 Start
+// var book = "A smarter \nway to learn JavaScript";
+// alert("I am trying to learn from the Book : "+book)
+//Question # 07 End
+
+//Question # 08 Start
+// document.write("Yah! I can write HTML content through JavaScript")
+//Question # 08 End
+
+
+// ! CHAPTER # 03
+
+
+//Question # 01 Start
+// var age = 26;
+// alert("I am "+ age + " years old")
+//Question # 01 End
+
+//Question # 02 Start
+// var visit = 23;
+// alert("You have visited this site "+ visit +" times") 
+//Question # 02 End
+
+//Question # 03 Start
+// var birthYear = 1998;
+// document.write("My Birth Year Is : "+ birthYear + "<br>")
+// document.write("Data type of my declared variable is "+ typeof birthYear)
+//Question # 03 End
+
+//Question # 04 Start
+// var visitorName = "John Doe";
+// var pTitle = "Suit(s)";
+// var quantity = 43;
+// document.write(visitorName + " ordered " + quantity+" "+ pTitle + " on XYZ clothing store");
+//Question # 04 Start
+
+// ! CHAPTER # 04
+
+
+//Question # 01 Start
+// var abc,def,xyz;
+//Question # 01 End
+
+//Question # 02 Start
+// LEGAL VARIABLE NAMES
+// var abc;
+// var num1;
+// var abc_23;
+// var $faraz;
+// var _123;
+
+// ILLEGAL VARIABLE NAMES
+// var -xyz;
+// var 1234;
+// var @abc;
+// var *987;
+// var hgn-23;
+//Question # 02 End
+
+//Question # 03 Start
+// document.write("<b>Rules for naming JS variables<b> <br> A) Variable names can only contain letters,numbers,dollar sign and underscores.<br> B) Variables must begin with a letters,dollar sign and underscores <br> C) Variable names are case Sensitive <br> D) Variable names should not be JS keywords");
+//Question # 03 End
+
+
+// ! CHAPTER # 05
+
+//Question # 01 Start
+// var num1 = 10;
+// var num2 =34;
+// var num = num1 + num2;
+// document.write(`Sum of ${num1} and ${num2} is ${num}`)
+//Question # 01 End
+
+//Question # 02 Start
+// ?FOR SUBTRACTION
+// var num1 = 10;
+// var num2 =34;
+// var num = num1 - num2;
+// document.write(`Subtraction of ${num1} and ${num2} is ${num} <br>`)
+
+// ?FOR  MULTIPLICATION
+// var num1 = 10;
+// var num2 =34;
+// var num = num1 * num2;
+// document.write(`Multipliaction of ${num1} and ${num2} is ${num}`)
+
+// ?FOR DIVISION
+// var num1 = 10;
+// var num2 =34;
+// var num = num1 / num2;
+// document.write(`Division of ${num1} and ${num2} is ${num}`)
+
+// ?FOR MODULUS
+// var num1 = 10;
+// var num2 =34;
+// var num = num1 % num2;
+// document.write(`Remainder of ${num1} and ${num2} is ${num}`)
+//Question # 02 End
+
+//Question # 03 Start
+// var value ;
+// document.write("Value after variable declaration is : " + value + "<br>")
+// value = 5;
+// document.write("Initial Value : " + value + "<br>");
+// ++value;
+// document.write("Value after increment is : " + value + "<br>");
+// value = value + 7;
+// document.write("Value after addition is : " + value + "<br>");
+// --value;
+// document.write("Value after decrement is : " + value + "<br>");
+// value = value % 3;
+// document.write("The remainder is : " + value);
+//Question # 03 End
+
+//Question # 04 Start
+// var ticketPrice = 600;
+// document.write("Total cost to buy 5 tickets to a movie is : "+ticketPrice * 5 + "PKR");
+//Question # 04 End
+
+//Question # 05 Start
+// ? TAble 0f 4
+// var table = 4;
+// document.write("4 x 1 = "+ table +"<br> 4 x 2 = " + table*2 + "<br> 4 x 3 = " + table*3 + "<br> 4 x 4 = " + table*4 + "<br> 4 x 5 = " + table*5 + "<br> 4 x 6 = " + table*6 + "<br> 4 x 7 = " + table*7 + "<br> 4 x 8 = " + table*8 +"<br> 4 x 9 = " + table*9 + "<br> 4 x 10 = " + table*10)
+//Question # 05 End
+
+//Question # 06 Start
+var celsius = 38;
+document.write(celsius+"<sup>o</sup>C is " + (100-32) * 5/9 + " <sup>o</sup>F <br>")
+var fahrenheit = 150;
+var formula = (fahrenheit -32) / (9/5);
+document.write(fahrenheit+"<sup>o</sup>F is " + formula + " <sup>o</sup>C")
+
+//Question # 06 End
